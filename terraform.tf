ec308a592498b4e51c6cd20ade66d546fb4d61b9
Himal Pandey <pandu.himal@gmail.com>
2025-05-15 11:36:57 +1000

    Add terraform and AWS provider blocks
        Configure terraform version constraints and
        AWS provider region.

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+# Configure Terraform version requirements and required providers.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.97.0"
+    }
+  }
+  required_version = "~> 1.11.4"
+}
+
+# Configure the AWS provider with the region specified by a variable
+provider "aws" {
+  region = var.region
+}
